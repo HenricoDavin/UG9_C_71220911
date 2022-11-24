06a74423906d76b06dfb634901afb5c8cfe29b6d
HenricoDavin <117893084+HenricoDavin@users.noreply.github.com>
2022-11-24 16:32:49 +0700

    Add files via upload

---
+++ 1_C_71220911.py
@@ -0,0 +1,8 @@
+nama = input(str ("Masukkan nama Anda :"))
+matkul = input(str ("Masukkan mata kuliah :"))
+grup = input(str ("Masukkan grup :"))
+#rumus
+print ("Haloo!",nama)
+print (nama,"tergabung dalam kelas",matkul,"pada grup",grup)
+
+
---
+++ 2_C_71220911.py
@@ -0,0 +1,8 @@
+bilpertama = int(input ("Masukkan kecepatan tempuh :"))
+bilkedua = int(input ("Masukkan waktu yang diperlukan :"))
+#rumus
+bensin = bilpertama*bilkedua/10
+biaya = bensin*15000
+print ("Teman anda mengisi sebanyak", bensin, "liter")
+print ("Biaya yang dikeluarkan untuk mengisi bensin adalah Rp. ", biaya)
+
